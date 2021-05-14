c25f8bdd9a330e5485a65d86088068c1401aae6b
The Dan <19521312@gm.uit.edu.vn>
2021-05-14 15:02:57 +0700

    initiate

---
+++ E_metro/ViewModel/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace E_metro.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        // mọi thứ xử lý sẽ nằm trong này
+        public MainViewModel()
+        {
+            //MessageBox.Show("Đã vào trong MainViewModel -> DataContext của mainwindow.xaml");
+        }
+    }
+}
