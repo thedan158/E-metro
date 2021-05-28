dfcd924452e1fce410150a368b253d64c4fd1f30
yang020501 <79902770+yang020501@users.noreply.github.com>
2021-05-28 21:50:22 +0700

    ticketupdateUI

---
+++ E_Metro/Ticket_kind.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace E_Metro
+{
+    /// <summary>
+    /// Interaction logic for Ticket_kind.xaml
+    /// </summary>
+    public partial class Ticket_kind : Window
+    {
+        public Ticket_kind()
+        {
+            InitializeComponent();
+        }
+    }
+}
