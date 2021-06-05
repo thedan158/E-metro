ea23f8be7b607f5f6622102e6c5e2b5d2d105aa6
yang020501 <79902770+yang020501@users.noreply.github.com>
2021-06-05 17:38:02 +0700

    fix Ticket

---
+++ E_Metro/ViewModel/Ticket_day.cs
@@ -0,0 +1,23 @@
+﻿using E_Metro.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Metro.ViewModel
+{
+    class Ticket_day : BaseViewModel
+    {
+        private ObservableCollection<Ticket> _DayList;
+        public ObservableCollection<Ticket> DayList { get => _DayList;set { _DayList = value; OnPropertyChanged(); } }
+        public Ticket_day()
+        {
+            _DayList = new ObservableCollection<Ticket>(DataProvider.Ins.DB.Tickets);
+        }
+
+
+    }
+
+}
